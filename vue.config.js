a17119bad17673659a973286e54d1ed835ac6ba0
Slee, Dominic <dominic.slee@news.com.au>
2019-05-30 15:52:43 +1000

    Attempts to do stuff

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,29 @@
 module.exports = {
   lintOnSave: false,
-  runtimeCompiler: true
+  runtimeCompiler: true,
+  configureWebpack: {
+    output: {
+      filename: '[name].js',
+      chunkFilename: '[name].js'
+    }
+  },
+  productionSourceMap: false,
+  chainWebpack: config => {
+    config.optimization.delete('splitChunks');
+    config.plugins.delete('NamedChunksPlugin');
+    config.plugins.delete('HashedModuleIdsPlugin');
+    console.log(config);
+
+    if(config.plugins.has('extract-css')) {
+      const extractCSSPlugin = config.plugin('extract-css')
+      extractCSSPlugin && extractCSSPlugin.tap(() => [{
+        filename: '[name].css',
+        chunkFilename: '[name].css'
+      }])
+    }
+    
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? 'https://raw.githubusercontent.com/domsleee/coreui-free-vue-admin-template/master/'
+    : '/',
 }
