09ff1399d61b6795a0c4a470f8fb9812e69c670c
Salvatore Balzano <salvatore.balzano@news.com.au>
2019-05-31 13:31:34 +1000

    badges

--- src/views/dashboard/mycode/BadgesDash.vue
+++ src/views/dashboard/mycode/BadgesDash.vue
@@ -6,11 +6,12 @@
               :rotate="-90"
               :size="100"
               :width="15"
-              :value=profileProgress
+              :value=20
               color="blue"
       >
         <v-img src='https://png.pngtree.com/element_origin_min_pic/17/04/12/c5490bc7210a7eb88f22804682570e9b.jpg' width=70 class='rounded-img' />
       </v-progress-circular>
+      <p class="badge-name"> (TOP READER)</p>
     </div>
 
     <div class="oneline">
@@ -18,11 +19,12 @@
               :rotate="-90"
               :size="100"
               :width="15"
-              :value=profileProgress
+              :value=80
               color="blue"
       >
         <v-img src='https://www.redwolf.in/image/cache/catalog/badges/tyrell-sigil-game-of-thrones-badge-india-700x700.jpg' width=80 class='rounded-img' />
       </v-progress-circular>
+      <p class="badge-name"> (SPORT FAN)</p>
     </div>
 
     <div class="oneline">
@@ -30,11 +32,12 @@
               :rotate="-90"
               :size="100"
               :width="15"
-              :value=profileProgress
+              :value=60
               color="blue"
       >
-        <v-img src='https://png.pngtree.com/element_origin_min_pic/17/04/12/c5490bc7210a7eb88f22804682570e9b.jpg' width=70 class='rounded-img' />
+        <v-img src='https://www.redwolf.in/image/catalog/badges/fire-and-blood-house-targaryen-game-of-thrones-badge-india.jpg' width=80 class='rounded-img' />
       </v-progress-circular>
+      <p class="badge-name"> (NEWS FAN)</p>
     </div>
 
     <div class="oneline">
@@ -42,11 +45,12 @@
               :rotate="-90"
               :size="100"
               :width="15"
-              :value=profileProgress
+              :value=100
               color="blue"
       >
-        <v-img src='https://png.pngtree.com/element_origin_min_pic/17/04/12/c5490bc7210a7eb88f22804682570e9b.jpg' width=70 class='rounded-img' />
+        <v-img src='https://www.redwolf.in/image/catalog/badges/khaleesi-daenerys-taragryen-game-of-thrones-badge-india.jpg' width=80 class='rounded-img' />
       </v-progress-circular>
+      <p class="badge-name"> (SUPER SHARER)</p>
     </div>
 
     <div class="oneline">
@@ -54,11 +58,12 @@
               :rotate="-90"
               :size="100"
               :width="15"
-              :value=profileProgress
+              :value=50
               color="blue"
       >
-        <v-img src='https://png.pngtree.com/element_origin_min_pic/17/04/12/c5490bc7210a7eb88f22804682570e9b.jpg' width=70 class='rounded-img' />
+        <v-img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSI-tJrMFYWWngI0I525ZeWCOeM2hA42cSxmedi8aW5eC-U88vnNA' width=80 class='rounded-img' />
       </v-progress-circular>
+      <p class="badge-name"> (GOSSIP QUEEN)</p>
     </div>
 
     <div class="oneline">
@@ -66,11 +71,12 @@
               :rotate="-90"
               :size="100"
               :width="15"
-              :value=profileProgress
+              :value=40
               color="blue"
       >
-        <v-img src='https://png.pngtree.com/element_origin_min_pic/17/04/12/c5490bc7210a7eb88f22804682570e9b.jpg' width=70 class='rounded-img' />
+        <v-img src='https://www.redwolf.in/image/catalog/badges/crown-game-of-thrones-badge-india.jpg' width=80 class='rounded-img' />
       </v-progress-circular>
+      <p class="badge-name"> (THE LOYAL)</p>
     </div>
 
     <div class="oneline">
@@ -78,11 +84,12 @@
               :rotate="-90"
               :size="100"
               :width="15"
-              :value=profileProgress
+              :value=90
               color="blue"
       >
-        <v-img src='https://png.pngtree.com/element_origin_min_pic/17/04/12/c5490bc7210a7eb88f22804682570e9b.jpg' width=70 class='rounded-img' />
+        <v-img src='https://cdn0.iconfinder.com/data/icons/smart-technology-rounded-volume-1-1/1000/gameplay_sharing-512.png' width=70 class='rounded-img' />
       </v-progress-circular>
+      <p class="badge-name"> (SQUARE EYES)</p>
     </div>
 
     <div class="oneline">
@@ -90,11 +97,12 @@
               :rotate="-90"
               :size="100"
               :width="15"
-              :value=profileProgress
+              :value=78
               color="blue"
       >
-        <v-img src='https://png.pngtree.com/element_origin_min_pic/17/04/12/c5490bc7210a7eb88f22804682570e9b.jpg' width=70 class='rounded-img' />
+        <v-img src='https://current-students.blogs.latrobe.edu.au/wp-content/uploads/sites/9/2017/02/mission-impossible-library.jpg' width=60 class='rounded-img' />
       </v-progress-circular>
+      <p class="badge-name"> ( MYSTERY )</p>
     </div>
 
   </div>
@@ -121,11 +129,18 @@ export default {
 </script>
 
 <style>
+  .badge-name {
+    margin-left: 10px;
+    margin-top: 10px;
+    font-weight: bold;
+    font-size: 10px;
+  }
   .oneline {
     width: 100px;
     height: 100px;
     display: inline-block;
-    margin-right: 30px;
+    margin-right: 90px;
+    margin-bottom: 50px;
   }
 .rounded-img {
     border-radius:50px;
